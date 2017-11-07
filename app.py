dbca836c974ef5f31ea84d40bb1ddccb56d0fb3c
oracle <oracle@centos7.localdomain>
2017-11-07 11:21:22 +0100

    added the list of urls for the application

--- app.py
+++ app.py
@@ -506,6 +506,22 @@ def list_all_certs():
 
     return render_template('list_all.html',result=certificate)
 
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 
+
+@app.route("/site-map")
+def site_map():
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    return render_template('site-map.html', links=links)
 if __name__ == '__main__':
     app.run()
